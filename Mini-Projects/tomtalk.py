74309b0a835f10d93ad515618c08cf40fbb3e407
Apsara Bishwokarma <apsarabishwokarma7@gmail.com>
2023-01-01 11:13:04 +0545

    mini projects

---
+++ Mini-Projects/tomtalk.py
@@ -0,0 +1,13 @@
+def tom_talk(text):
+    print(f"Tom: {text}")
+
+
+# make Tom say some things
+tom_talk("Hello, how are you?")
+tom_talk("I'm doing well, thanks for asking.")
+tom_talk("I'm just a computer program, so I don't have feelings or emotions like a human.")
+
+# This code defines a function called tom_talk that takes a piece of text as
+# an argument and prints it out with the prefix "Tom: ".
+# To make Tom say some things, we call the tom_talk function with different pieces of text as arguments.
+# This will print out the text preceded by "Tom: ".
